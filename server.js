448e4c451792dfdac62eb35be0b797f57e86c9e6
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-09 20:48:24 -0800

    add catch all route

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 const app = express();
 app.use(cors());
 app.get('/weather', handleGetWeather)
+app.get('/*', ((req, res) => res.status(404).send('route not found')));
 
 const PORT = process.env.PORT;
 
