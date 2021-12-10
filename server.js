b53f8b74c97507496d1461b6cd0340b1a5d2ec6e
Beau Hibbert <beauhibbert789@gmail.com>
2021-12-09 23:59:30 -0800

    backend refactoring

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('dotenv').config();
+const handleGetWeather = require('./weather.js');
+const handleGetMovies = require('./movies.js');
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
@@ -13,51 +15,5 @@ app.get('/*', ((req, res) => res.status(404).send('route not found')));
 
 const PORT = process.env.PORT;
 
-function handleGetWeather(request, response) {
-  const dailyWeatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I`
-  axios.get(dailyWeatherURL)
-    .then(responseObject => {
-      // any response object that comes back from an Axios request will have a "data" property
-      // the shape of responseObject.data is then dictated by the API
-      // Weatherbit API sends back a blob with a "data" property on it as well, which is why we have to access the stuff we care about at responseObject.data.data
-      let weatherDescription = responseObject.data.data.map(dataBlob => new Forecast(dataBlob));
-      response.status(200).send(weatherDescription);
-    })
-    .catch(error => {
-      console.error(error.message);
-      response.status(500).send('Sorry, something went wrong! (Status code: 500)');
-    });
-}
-
-async function handleGetMovies(req, res) {
-  const { city_name } = req.query;
-  try {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${city_name}&page=1&include_adult=false`;
-    const movieResponse = await axios.get(url);
-    const cleanedMovies = movieResponse.data.results.map(movie => new Movie(movie));
-    res.send(cleanedMovies);
-  } catch (e) {
-    res.status(500).send('server error')
-  }
-}
-
-class Forecast {
-  constructor(obj) {
-    this.date = obj.datetime;
-    this.description = `A high of ${obj.max_temp}, a low of ${obj.low_temp}, and ${obj.weather.description.toLowerCase()}.`
-  }
-}
-
-class Movie {
-  constructor(obj) {
-    this.title = obj.title;
-    this.overview = obj.overview;
-    this.avg_votes = obj.vote_average;
-    this.total_votes = obj.vote_count;
-    this.image_url = obj.poster_path ? `https://image.tmdb.org/t/p/w500${obj.poster_path}` : `https://www.reelviews.net/resources/img/default_poster.jpg`;
-    this.popularity = obj.popularity;
-    this.released_on = obj.release_date;
-  }
-}
 
 app.listen(PORT, () => console.log('server is listening on port ', PORT));
